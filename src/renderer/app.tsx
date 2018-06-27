2188999b977dbbcd9e7546fcc9f9bd57af879e10
danzel <danzel@localhost.geek.nz>
2018-06-27 19:55:42 +1200

    Drop in blueprint, react-router (not used yet)

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,24 +1,34 @@
 import * as React from 'react';
 import { hot } from 'react-hot-loader';
-import { Button } from '@blueprintjs/core';
 
 interface State {
-    time: number;
 }
 
 class App extends React.Component<{}, State>{
+	constructor(props: {}) {
+		super(props);
 
-    constructor(props: {}) {
-        super(props);
+		this.state = {
+		}
+	}
 
-        this.state = {
-            time: Date.now()
-        }
-    }
-
-    render() {
-        return <div><Button text="hi"/> App3 {this.state.time}</div>
-    }
+	render() {
+		return (
+			<div>
+				<nav className="pt-navbar pt-dark">
+					<div className="pt-navbar-group pt-align-left">
+						<div className="pt-navbar-heading">budgetless</div>
+						<input className="pt-input" placeholder="Search something..." type="text" />
+					</div>
+					<div className="pt-navbar-group pt-align-right">
+						<button className="pt-button pt-minimal pt-icon-home">Home</button>
+						<button className="pt-button pt-minimal pt-icon-import">Import</button>
+						<span className="pt-navbar-divider"></span>
+						<button className="pt-button pt-minimal pt-icon-cog"></button>
+					</div>
+				</nav>
+			</div>);
+	}
 }
 
 export default hot(module)(App)
