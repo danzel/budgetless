63b4cdfe16dd3fbe3693dce2681280a373cce613
danzel <danzel@localhost.geek.nz>
2018-06-30 16:03:36 +1200

    Specifying entities manually like this works

--- src/services/database.ts
+++ src/services/database.ts
@@ -2,6 +2,12 @@ import { injectable } from "inversify";
 import { createConnection, Connection, Repository } from "typeorm";
 import { BankAccount } from "../entities";
 
+let config = require('../../ormconfig.json')
+config.entities = [
+	BankAccount
+];
+config.migrations = [];
+
 @injectable()
 export class Database {
 	constructor(
@@ -12,7 +18,7 @@ export class Database {
 
 export async function createDatabase() {
 	console.log("Creating the database");
-	let connection = await createConnection();
+	let connection = await createConnection(config);
 	let bankAccounts = connection.getRepository(BankAccount);
 
 	return new Database(connection, bankAccounts);
