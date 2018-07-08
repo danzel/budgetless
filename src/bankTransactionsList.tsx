1e52fe2b8a2522b40caa920ad94b13f9791dbf52
danzel <danzel@localhost.geek.nz>
2018-07-08 21:16:33 +1200

    Basic transaction table view using react-table

--- src/bankTransactionsList.tsx
+++ src/bankTransactionsList.tsx
@@ -1,22 +1,41 @@
 import * as React from 'react';
 import { Navbar, NavbarGroup, Alignment, ButtonGroup, Button, MenuItem, Checkbox, Popover, Menu, PopoverInteractionKind, NavbarDivider } from '@blueprintjs/core';
 import { Select } from '@blueprintjs/select';
-import { BankAccount, Category } from './entities';
+import { BankAccount, Category, BankTransaction } from './entities';
 import { lazyInject, Services, Database } from './services';
+import ReactTable, { Column } from 'react-table';
+import * as dayjs from 'dayjs';
+import { In, IsNull, FindConditions } from 'typeorm';
 
+interface DateRange {
+	name: string;
+	start: dayjs.Dayjs;
+	end: dayjs.Dayjs;
+}
 
 let CategorySelect = Select.ofType<Category>();
 
 const everyCategory = new Category("Everything");
+everyCategory.categoryId = -1;
 const uncategorisedCategory = new Category("Uncategorised");
+uncategorisedCategory.categoryId = -2;
+
+const dateRanges = new Array<DateRange>(
+	{ name: 'This Month', start: dayjs().startOf('month'), end: dayjs().endOf('month') },
+	{ name: 'Last Month', start: dayjs().subtract(1, 'month').startOf('month'), end: dayjs().subtract(1, 'month').endOf('month') },
+	{ name: 'This Year', start: dayjs().startOf('year'), end: dayjs().endOf('year') },
+	{ name: 'Last Year', start: dayjs().subtract(1, 'year').startOf('year'), end: dayjs().subtract(1, 'year').endOf('year') },
+);
 
 interface State {
 	accounts?: BankAccount[];
 	categories?: Category[];
 
 	selectedAccounts: BankAccount[];
-
 	selectedCategory: Category;
+	selectedDateRange: DateRange;
+
+	transactions: BankTransaction[];
 }
 
 
@@ -29,7 +48,9 @@ export class BankTransactionsList extends React.Component<{}, State> {
 
 		this.state = {
 			selectedAccounts: [],
-			selectedCategory: everyCategory
+			selectedCategory: everyCategory,
+			selectedDateRange: dateRanges[0],
+			transactions: []
 		};
 
 		this.load();
@@ -55,25 +76,64 @@ export class BankTransactionsList extends React.Component<{}, State> {
 	private toggleAccount(account: BankAccount) {
 		if (this.state.selectedAccounts.some(a => a == account)) {
 			this.setState({
-				selectedAccounts: this.state.selectedAccounts.filter(a => a != account)
-			});
+				selectedAccounts: this.state.selectedAccounts.filter(a => a != account),
+				transactions: []
+			}, () => this.loadTransactions());
 		} else {
 			let a = this.state.selectedAccounts.slice();
 			a.push(account);
 			this.setState({
-				selectedAccounts: a
-			});
+				selectedAccounts: a,
+				transactions: []
+			}, () => this.loadTransactions());
 		}
 	}
 
 	private selectCategory(c: Category) {
 		this.setState({
-			selectedCategory: c
+			selectedCategory: c,
+			transactions: []
+		}, () => this.loadTransactions());
+	}
+
+	private selectDateRange(dateRange: DateRange) {
+		this.setState({
+			selectedDateRange: dateRange,
+			transactions: []
+		}, () => this.loadTransactions());
+	}
+
+	private async loadTransactions() {
+		let db = await this.database;
+
+		let where: FindConditions<BankTransaction> = {
+			bankAccount: In(this.state.accounts!.map(a => a.bankAccountId)),
+		};
+		if (this.state.selectedCategory.categoryId == everyCategory.categoryId) {
+		} else if (this.state.selectedCategory.categoryId == uncategorisedCategory.categoryId) {
+			where.category = IsNull();
+		} else {
+			where.category = this.state.selectedCategory;
+		}
+
+		let transactions = await db.transactions.find({
+			where,
+			relations: [
+				'bankAccount',
+				'category'
+			],
+			order: {
+				date: 'DESC'
+			}
+		});
+		console.log(transactions.length);
+
+		this.setState({
+			transactions
 		});
 	}
 
 	render() {
-
 		if (!this.state.accounts || !this.state.categories) {
 			return <div>Loading</div>;
 		}
@@ -87,13 +147,31 @@ export class BankTransactionsList extends React.Component<{}, State> {
 			}
 		}
 
+		const columns: Column[] = [
+			{ Header: 'Date', accessor: 'date', Cell: d => (d.value as dayjs.Dayjs).format("YYYY-MM-DD") },
+			{ Header: 'Amount', accessor: 'amount' },
+			{ Header: 'Description', accessor: 'description' },
+			{ Header: 'Category', accessor: 'category', Cell: d => d.value ? (d.value as Category).name : "NOT SET" },
+			{ Header: 'Account', accessor: 'bankAccount', Cell: d => (d.value as BankAccount).name }
+		];
+
 		return <div className="bank-transactions-list">
 			<Navbar>
 				<NavbarGroup align={Alignment.LEFT}>
 
 					<ButtonGroup>
 						<Button icon="chevron-left" />
-						<Button text="TODO Show selected" />
+						<Popover>
+							<Button icon="calendar" text={this.state.selectedDateRange.name} />
+							<Menu>
+								{dateRanges.map(d => <MenuItem
+									key={d.name}
+									text={d.name}
+									onClick={() => this.selectDateRange(d)}
+								/>
+								)}
+							</Menu>
+						</Popover>
 						<Button icon="chevron-right" />
 					</ButtonGroup>
 
@@ -124,7 +202,11 @@ export class BankTransactionsList extends React.Component<{}, State> {
 
 				</NavbarGroup>
 			</Navbar>
-			
+
+			<ReactTable
+				data={this.state.transactions}
+				columns={columns}
+			/>
 		</div>;
 	}
 }
